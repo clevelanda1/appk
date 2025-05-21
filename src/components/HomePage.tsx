782e17b998bf4f4a098364e314ef5a15efe5b22f
clevelanda1 <clevelandaus1@gmail.com>
2025-05-21 02:17:49 -0500

    hide 'Apple Pay Prank' on HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -489,13 +489,13 @@ const HomePage: React.FC = () => {
         </>
       )}
       
-      <div className="absolute bottom-2 w-full flex flex-col items-center space-y-3" 
+      {/*<div className="absolute bottom-2 w-full flex flex-col items-center space-y-3" 
           style={{ 
             marginBottom: 'env(safe-area-inset-bottom, 0px)',
             paddingBottom: '0px' // Add extra padding at the bottom
           }}>
         <p className="text-gray-500 text-xs font-medium">Apple Pay Prank v1.0</p>
-      </div>
+        </div>*/}
       
       <style jsx>{`
         @keyframes shineEffect {
