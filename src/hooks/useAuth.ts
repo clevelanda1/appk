7214439ef3ab1bfb59ff1bee629caf2d722a051f
clevelanda1 <clevelandaus1@gmail.com>
2025-05-23 15:38:05 -0500

    5

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -72,7 +72,7 @@ export function useAuth() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/pricing`,
+        redirectTo: `${window.location.origin}/cardScreen`,
         queryParams: {
           access_type: 'offline',
           prompt: 'consent'
